b338c6c1a9a43b538f5fb7d71d2e34dcd2f9a631
Douglas <dougdwfp@gmail.com>
2021-10-31 20:17:13 -0300

    ajustes

--- Trab5/Ex01.cbl
+++ Trab5/Ex01.cbl
@@ -1,25 +1,25 @@
-*>****************************************************************
-*> Author:
-*> Date:
-*> Purpose:
-*> Tectonics: cobc
-*>****************************************************************
-IDENTIFICATION DIVISION.
-PROGRAM-ID. YOUR-PROGRAM-NAME.
-DATA DIVISION.
-FILE SECTION.
-WORKING-STORAGE SECTION.
-77 X PIC 99999.
-77 Y PIC 99999.
-PROCEDURE DIVISION.
-MAIN-PROCEDURE.
-    PERFORM VARYING X FROM 1 BY 1
-     UNTIL X > 3
-     DISPLAY X ' ' WITH NO ADVANCING
-     PERFORM VARYING Y FROM 1 BY 1
-      UNTIL Y > 5
-      DISPLAY Y ' ' WITH NO ADVANCING
-     END-PERFORM
-     DISPLAY ' '
-    END-PERFORM.
-END PROGRAM YOUR-PROGRAM-NAME.
+        *>****************************************************************
+        *> Author:
+        *> Date:
+        *> Purpose:
+        *> Tectonics: cobc
+        *>****************************************************************
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. YOUR-PROGRAM-NAME.
+        DATA DIVISION.
+        FILE SECTION.
+        WORKING-STORAGE SECTION.
+        77 X PIC 99999.
+        77 Y PIC 99999.
+        PROCEDURE DIVISION.
+        MAIN-PROCEDURE.
+            PERFORM VARYING X FROM 1 BY 1
+             UNTIL X > 3
+             DISPLAY X ' ' WITH NO ADVANCING
+             PERFORM VARYING Y FROM 1 BY 1
+              UNTIL Y > 5
+              DISPLAY Y ' ' WITH NO ADVANCING
+             END-PERFORM
+             DISPLAY ' '
+            END-PERFORM.
+        END PROGRAM YOUR-PROGRAM-NAME.
